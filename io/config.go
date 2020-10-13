ed8a4d5937aacf35b3060354c65780c839ed4e7c
heroslender <bruno.martins1998@hotmail.com>
2020-10-13 21:05:20 +0100

    Refactor the recover func to catch & rethrow the err

--- io/config.go
+++ io/config.go
@@ -1,36 +1,51 @@
 package io
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"runtime/debug"
 	"strings"
-
-	"github.com/sirupsen/logrus"
 )
 
-func recoverConfigAction() {
-	if r := recover(); r != nil {
-		logrus.Errorf("Recovered from some config action: %s\n", r)
-		debug.PrintStack()
+func parseRecoverErr(r interface{}) (err error) {
+	switch x := r.(type) {
+	case string:
+		err = fmt.Errorf(x)
+	case error:
+		err = x
+	default:
+		err = fmt.Errorf("unknown error from panic: %s", x)
 	}
+
+	return
 }
 
 // SaveConfig saves the provided config into the file with the given name
 func SaveConfig(cfg interface{}, file string) (err error) {
-	defer recoverConfigAction()
+	defer func() {
+		if r := recover(); r != nil {
+			err = parseRecoverErr(r)
+		}
+	}()
 
 	file = validateFileName(file)
 
-	os.MkdirAll(filepath.Dir(file), os.ModePerm)
+	if err = os.MkdirAll(filepath.Dir(file), os.ModePerm); err != nil {
+		return
+	}
+
 	err = ioutil.WriteFile(file, hclEnconde(cfg), 0777)
 	return
 }
 
 // LoadConfig loads a config from the given file, into the interface
 func LoadConfig(file string, cfg interface{}) (err error) {
-	defer recoverConfigAction()
+	defer func() {
+		if r := recover(); r != nil {
+			err = parseRecoverErr(r)
+		}
+	}()
 
 	file = validateFileName(file)
 	src, err := ioutil.ReadFile(file)
